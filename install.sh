767157e64ba61bb492d6cd339ed6e92e0611e852
Cristian Dinu <cristian.dinu@supreva.com>
2024-05-23 13:18:29 +0300

    feat #2: added git-prompt

--- install.sh
+++ install.sh
@@ -7,5 +7,8 @@ cp ./.config/nvim/init.vim ~/.config/nvim/init.vim
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
   "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 
+curl -fLo ~/.bin/git-prompt.sh --create-dirs \
+  "https://raw.githubusercontent.com/magicmonty/bash-git-prompt/2.7.1/gitprompt.sh"
+
 cp ./bashrc ~/.bashrc_local
 echo "source ~/.bashrc_local" >> ~/.bashrc
